b0b65d81f4b29aae692b39203b43406750f22312
Vladislav Verkhovtsev <vlad.verkhovtsev@gmail.com>
2023-01-30 00:24:42 +0300

    updating App.java with playerHello method

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,11 @@
 package hexlet.code;
 
+import static hexlet.code.Cli.playerHello;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        System.out.println("May I have your name?");
+        playerHello();
     }
 }
