65a9eda895569da7f6b1cc19ebd3f7e437b2ca75
Vladislav Verkhovtsev <vlad.verkhovtsev@gmail.com>
2023-01-30 00:27:51 +0300

    fixed greetings

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,7 +5,7 @@ import static hexlet.code.Cli.playerHello;
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
-        System.out.println("May I have your name?");
+        System.out.print("May I have your name? ");
         playerHello();
     }
 }
