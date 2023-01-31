ec71e0cec74fa20d8bea9fc82034822a9757dc45
Vladislav Verkhovtsev <vlad.verkhovtsev@gmail.com>
2023-01-31 11:25:32 +0300

    fixed CRLF to LF separator

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,11 @@
 package hexlet.code;
 
-import static hexlet.code.Cli.playerHello;
+import static hexlet.code.Cli.playerGreetings;
 
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
-        playerHello();
+        playerGreetings();
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,12 +3,12 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void playerHello() {
-        Scanner scanner = new Scanner(System.in);
-        String playerName = scanner.nextLine();
+    public static void playerGreetings() {
+        Scanner sc = new Scanner(System.in);
+        String playerName = sc.nextLine();
 
         System.out.println("Hello, " + playerName + "!");
 
-        scanner.close();
+        sc.close();
     }
 }
