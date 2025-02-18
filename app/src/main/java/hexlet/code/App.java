42e2be4c76ed92914e9b2f5d9712b906ab3162f5
trustmycode <vlad.verkhovtsev@gmail.com>
2025-02-18 14:02:59 +0300

    fix

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -14,19 +14,19 @@ import java.io.InputStreamReader;
 public class App {
 
   public static void main(String[] args) throws IOException {
-    System.out.println("Please enter the game number and press Enter.");
-    System.out.println("1 - Greet");
-    System.out.println("2 - Even");
-    System.out.println("3 - Calc");
-    System.out.println("4 - Gcd");
-    System.out.println("5 - Progression");
-    System.out.println("6 - Prime");
-    System.out.println("0 - Exit");
-    System.out.print("Your choice: ");
-
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    int numberOfGame = Integer.parseInt(bufferedReader.readLine());
     while (true) {
+      System.out.println("Please enter the game number and press Enter.");
+      System.out.println("1 - Greet");
+      System.out.println("2 - Even");
+      System.out.println("3 - Calc");
+      System.out.println("4 - Gcd");
+      System.out.println("5 - Progression");
+      System.out.println("6 - Prime");
+      System.out.println("0 - Exit");
+      System.out.print("Your choice: ");
+
+      BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+      int numberOfGame = Integer.parseInt(bufferedReader.readLine());
       switch (numberOfGame) {
         case (1) -> playerGreetings(bufferedReader);
         case (2) -> evenGame(bufferedReader);
