fe6bf48752939986ed0845afaa68ba9e13abb138
trustmycode <vlad.verkhovtsev@gmail.com>
2025-02-18 16:17:35 +0300

    Update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -13,28 +13,26 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) throws IOException {
-        while (true) {
-            System.out.println("Please enter the game number and press Enter.");
-            System.out.println("1 - Greet");
-            System.out.println("2 - Even");
-            System.out.println("3 - Calc");
-            System.out.println("4 - Gcd");
-            System.out.println("5 - Progression");
-            System.out.println("6 - Prime");
-            System.out.println("0 - Exit");
-            System.out.print("Your choice: ");
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("3 - Calc");
+        System.out.println("4 - Gcd");
+        System.out.println("5 - Progression");
+        System.out.println("6 - Prime");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
 
-            Scanner scanner = new Scanner(System.in);
-            int numberOfGame = scanner.nextInt();
-            switch (numberOfGame) {
-                case (1) -> playerGreetings();
-                case (2) -> evenGame();
-                case (3) -> calcGame();
-                case (4) -> gcdGame();
-                case (5) -> progressionGame();
-                case (6) -> primeGame();
-                case (0) -> System.exit(0);
-            }
+        Scanner scanner = new Scanner(System.in);
+        int numberOfGame = scanner.nextInt();
+        switch (numberOfGame) {
+            case (1) -> playerGreetings();
+            case (2) -> evenGame();
+            case (3) -> calcGame();
+            case (4) -> gcdGame();
+            case (5) -> progressionGame();
+            case (6) -> primeGame();
+            case (0) -> System.exit(0);
         }
     }
 }
